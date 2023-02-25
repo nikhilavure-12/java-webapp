61c04d8221808f9c2675ae8de7231e08074bddba
Nikhila <nikhila@gmail.com>
2023-02-25 22:44:42 +0530

    Maven Project

---
+++ java-web-app/src/main/java/com/Nikhila/App.java
@@ -0,0 +1,13 @@
+package com.Nikhila;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
